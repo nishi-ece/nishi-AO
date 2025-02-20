235ce072a605483f6db6ad3e3f11fe71443b1acd
nishi <nithaesther@Nithas-MacBook-Air.local>
2025-02-20 18:31:57 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent form submission
+    alert('Thank you for contacting me! I’ll get back to you soon.');
+});
